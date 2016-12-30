a8f29db9708d35bbd8e5e6fa0cd8b2b9d7ac0d8f
jarrattisted <jarratt@helpdocs.io>
2016-12-30 08:39:37 +0000

    dynamic node url get string

--- js/app.js
+++ js/app.js
@@ -1,18 +1,22 @@
-// Problem: Get status of station from TFL when typed in
-// Solution: Use Node to GET status from TFL on certain stations
+// Problem: Get status of line from TFL when typed in
+// Solution: Use Node to GET status from TFL on certain lines
+// GET	/Line/{ids}
+
+var http = require('http');
 
 // TFL API Keys
 var appID = 'fcbd4b88';
 var authKey = '96e979501f4df83bce0ed55cf3555411';
-
-function printStatus(station, status) {
-  var message = 'There are currently ' + status + ' on the ' + station + ' line.';
-  console.log(message);
-}
-
-printStatus('Victoria', 'minor delays');
+var tubeLine = 'victoria';
 
 // Connect to API using TFL keys
-// Read the data
-// Parse the data
-// Print the data
+var request = http.get('http://api.tfl.gov.uk/line/' + tubeLine + '?app_id=' + appID +
+              '&app_key=' + authKey, function (response) {
+  // Read the data
+  response.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
+
+  // Parse the data
+  // Print the data
+});
